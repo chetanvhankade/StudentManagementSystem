85eb84a32ff652a278ed0bc0cbc7c920abaf4b57
Chetan <chetanvhankade86@gmail.com>
2025-07-15 19:09:26 +0530

    Update and rename Hibernate Util.java to HibernateUtil.java

--- HibernateUtil.java
+++ HibernateUtil.java
@@ -2,7 +2,7 @@ package org.example;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
-public class Util
+public class HibernateUtil
 {
     private static final EntityManagerFactory entity = Persistence.createEntityManagerFactory("StudentPU");
 
